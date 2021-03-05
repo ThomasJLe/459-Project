94af31f7c017903c9528dfea8fe3a2a3b85a234a
Donavie Ordonez <donavielove@csu.fullerton.edu>
2021-03-05 15:05:35 -0800

    adding comments and changed summation of input total

--- starter code/TxHandler.java
+++ starter code/TxHandler.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class TxHandler {
 
 	private UTXOPool uPool;
@@ -13,50 +14,60 @@ public class TxHandler {
 		double inValue = 0.0;
 		int i = 0;
 		HashSet<UTXO> utSeen = new HashSet<UTXO>();
-			
-			// all outputs claimed by tx are in the current UTXO pool 
-		for(Transaction.Input in : tx.getInputs()) {
+
+		// all outputs claimed by tx are in the current UTXO pool
+		for (Transaction.Input in : tx.getInputs()) {
+			// Creating an object of UTXO to manipulate
 			UTXO ut = new UTXO(in.prevTxHash, in.outputIndex);
-			if (!this.uPool.contains(ut)) { 
+
+			// (1) Return true if all outputs claimed by tx are in the current UTXO pool
+			if (!this.uPool.contains(ut)) {
 				return false;
 			}
 
-			double preVal = uPool.getTxOutput(ut).value;
-			inValue += preVal;
+			// double preVal = uPool.getTxOutput(ut).value;
+			// inValue += preVal;
+			// Add to the input total
+			inValue += uPool.getTxOutput(ut).value;
 
-				// no UTXO is claimed multiple times by tx
-			if (utSeen.contains(ut)) { 
+			// (3) Return true if no UTXO is claimed multiple times by tx
+			if (utSeen.contains(ut)) 
 				return false;
-			}
+			// Add the UTXO to the Hash set
 			utSeen.add(ut);
 
-				// the signatures on each input of tx are valid
-			if(!uPool.getTxOutput(ut).address.verifySignature(tx.getRawDataToSign(i), in.signature)) { 
+			// (2) Return true if the signatures on each input of tx are valid
+			if (!uPool.getTxOutput(ut).address.verifySignature(tx.getRawDataToSign(i), in.signature)) 
 				return false;
-			}
+			
+			// Increment the index
 			i++;
 		}
 
-			// all of tx�s output values are non-negative
+		// all of tx's output values are non-negative
+		// Forloop to go through Transaction outputs
 		for (Transaction.Output out : tx.getOutputs()) {
-			if (out.value < 0.0) { 
+			// (4) Return true if all of tx's output values are non-negative
+			if (out.value < 0.0)
 				return false;
-			}
+
+			// Add to the output total
 			outValue += out.value;
 		}
 
-			// the sum of tx�s input values is greater than or equal to the sum of its output values
-		if (inValue < outValue) { 
+		// (5) Return true if the sum of tx's input values is greater than or equal to
+		// the sum of its output values
+		if (inValue < outValue)
 			return false;
-		}
-			// all were true
+			
+		// Else, returns true for all statements
 		return true;
 	}
 
-	/* Handles each epoch by receiving an unordered array of proposed 
-	 * transactions, checking each transaction for correctness, 
-	 * returning a mutually valid array of accepted transactions, 
-	 * and updating the current UTXO pool as appropriate.
+	/*
+	 * Handles each epoch by receiving an unordered array of proposed transactions,
+	 * checking each transaction for correctness, returning a mutually valid array
+	 * of accepted transactions, and updating the current UTXO pool as appropriate.
 	 */
 	public Transaction[] handleTxs(Transaction[] possibleTxs) {
 		// (1) Return only valid transactions
@@ -73,35 +84,34 @@ public class TxHandler {
 			transCount = TransXs.size();
 			HashSet<Transaction> forRemove = new HashSet<Transaction>();
 			for (Transaction Tx : TransXs) {
-				if(!isValidTx(Tx)) {
+				if (!isValidTx(Tx)) {
 					continue;
 				}
 
 				valid.add(Tx);
-				
-				for(Transaction.Input in : Tx.getInputs()) {
+
+				for (Transaction.Input in : Tx.getInputs()) {
 					UTXO Nutxo = new UTXO(in.prevTxHash, in.outputIndex);
 					this.uPool.removeUTXO(Nutxo);
 				}
 
 				byte[] Hash = Tx.getHash();
 				int i = 0;
-				
+
 				for (Transaction.Output output : Tx.getOutputs()) {
 					UTXO Nutxo = new UTXO(Hash, i);
 					i++;
-					this.uPool.addUTXO(Nutxo,output);
-				}				
+					this.uPool.addUTXO(Nutxo, output);
+				}
 				forRemove.add(Tx);
 
 			}
 
-			for (Transaction Tx : forRemove){
+			for (Transaction Tx : forRemove) {
 				TransXs.remove(Tx);
 			}
 
-
-		} while (transCount != TransXs.size()  && transCount != 0);
+		} while (transCount != TransXs.size() && transCount != 0);
 		return valid.toArray(new Transaction[valid.size()]);
 	}
 }
