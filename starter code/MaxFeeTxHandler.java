0ab62aa8e1e3e49e17404d7953436c97e7e4ff19
Donavie Ordonez <donavielove@csu.fullerton.edu>
2021-03-07 17:43:03 -0800

    added comments

--- starter code/MaxFeeTxHandler.java
+++ starter code/MaxFeeTxHandler.java
@@ -9,7 +9,7 @@ public class MaxFeeTxHandler {
 	}
 
 	public boolean isValidTx(Transaction tx) {
-
+		// Initializing variables
 		double outValue = 0.0;
 		double inValue = 0.0;
 		int i = 0;
@@ -62,56 +62,66 @@ public class MaxFeeTxHandler {
 		return true;
 	}
 
-	
-	 private double taxFees(Transaction tx) {
-		 
-	        double inputSum = 0;
-	        double outputSum = 0;
-	        
-	        for (Transaction.Input in : tx.getInputs()) {
-	            UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
-	            if (!uPool.contains(utxo) || !isValidTx(tx)) continue;
-	            Transaction.Output txOutput = uPool.getTxOutput(utxo);
-	            inputSum += txOutput.value;
-	        }
-	        for (Transaction.Output out : tx.getOutputs()) {
-	            outputSum += out.value;
-	        }
-	        return inputSum - outputSum;
-	    }
-	
-	
-	
+	private double taxFees(Transaction tx) {
+		// Initializing variables
+		double inputSum = 0;
+		double outputSum = 0;
+
+		// Looping through the transaction inputs
+		for (Transaction.Input in : tx.getInputs()) {
+			// Creating an object of UTXO to manipulate
+			UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
+			if (!uPool.contains(utxo) || !isValidTx(tx))
+				continue;
+			Transaction.Output txOutput = uPool.getTxOutput(utxo);
+			inputSum += txOutput.value;
+		}
+		// Looping through transaction outputs to sum them up
+		for (Transaction.Output out : tx.getOutputs()) {
+			outputSum += out.value;
+		}
+
+		// Returns the tx fee
+		return inputSum - outputSum;
+	}
+
+	// ​This method finds a set of transactions with maximum total transaction fees
 	public Transaction[] handleTxs(Transaction[] possibleTxs) {
-		 
-        Set<Transaction> sortByFees = new TreeSet<>((tx1, tx2) -> {
-            double tax1 = taxFees(tx1);
-            double tax2 = taxFees(tx2);
-            return Double.valueOf(tax2).compareTo(tax1);
-        });
- 
-        Collections.addAll(sortByFees, possibleTxs);
- 
-        Set<Transaction> acceptedTax = new HashSet<>();
-        for (Transaction tx : sortByFees) {
-        	
-            if (isValidTx(tx)) {
-            	
-                acceptedTax.add(tx);
-                
-                for (Transaction.Input in : tx.getInputs()) {
-                    UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
-                    uPool.removeUTXO(utxo);
-                }
-                
-                for (int i = 0; i < tx.numOutputs(); i++) {
-                    Transaction.Output out = tx.getOutput(i);
-                    UTXO utxo = new UTXO(tx.getHash(), i);
-                    uPool.addUTXO(utxo, out);
-                }
-            }
-        }
-        Transaction[] validTxArray = new Transaction[acceptedTax.size()];
-        return acceptedTax.toArray(validTxArray);
-    }
+		// Creating a set that sorts the fees
+		Set<Transaction> sortByFees = new TreeSet<>((tx1, tx2) -> {
+			double tax1 = taxFees(tx1);
+			double tax2 = taxFees(tx2);
+			return Double.valueOf(tax2).compareTo(tax1);
+		});
+
+		// Adding the fees and possible transactions
+		Collections.addAll(sortByFees, possibleTxs);
+
+		Set<Transaction> acceptedTax = new HashSet<>();
+		for (Transaction tx : sortByFees) {
+			// If transaction is valid, add it to the set
+			if (isValidTx(tx)) {
+
+				acceptedTax.add(tx);
+
+				// Looping through the Transaction inputs to remove the UTXO
+				for (Transaction.Input in : tx.getInputs()) {
+					UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
+					uPool.removeUTXO(utxo);
+				}
+
+				// Looping through the number of outputs and adding to uPool
+				for (int i = 0; i < tx.numOutputs(); i++) {
+					Transaction.Output out = tx.getOutput(i);
+					UTXO utxo = new UTXO(tx.getHash(), i);
+					uPool.addUTXO(utxo, out);
+				}
+			}
+		}
+		// Creating an array of type Transaction adding the valid transactions
+		Transaction[] validTxArray = new Transaction[acceptedTax.size()];
+
+		// Returning the accepted transactions
+		return acceptedTax.toArray(validTxArray);
+	}
 }
